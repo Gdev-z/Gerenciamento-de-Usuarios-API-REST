3ce10c2d25488803e24b33017db9231692a2976a
Gabriel <gabrielzanon.dev@gmail.com>
2026-04-19 19:49:52 -0300

    Inicial

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components'
+
+export const  Container = styled.div`
+    background-color: #181f36;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    padding: 20px;
+    height: 100vh
+`
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    max-width: 500px;
+`
+export const Title = styled.h2`
+    color: #ffffff;
+    text-align: center;
+    font-size: 38px;
+    font-style: normal;
+    line-weight: 600;
+`
+
+export const ContainerInputs = styled.div`
+    display: flex;
+    gap: 20px;
+`
+
+export const Input = styled.input`
+    border-radius: 10px;
+    border: 1px solid #d2dae2;
+    background-color: #fff;
+    padding: 12px 20px;
+    outline: none;
+    width: 100%;
+`
+
+export const InputLabel = styled.label`
+    color: #fff;
+    font-size: 11px;
+    font-weight: 600;
+
+    span{
+    color: #ef4f45;
+    font-weight: bold;  
+    }
+`
+
+export const Button = styled.button`
+    border: none;
+    background: linear-gradient(180deg, #FE7E5D 0%, #FF6378 100%);
+    font-size: 16px;
+    color: #fff;
+    padding: 16px 32px;
+    width: fit-content;
+    cursor: pointer;
+    border-radius: 30px;
+
+    &:hover{
+        opacity: 0.8;
+    }
+    &:active{
+        opacity: 0.5;
+    }
+`
